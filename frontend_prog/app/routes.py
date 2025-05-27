111dc9213569f8c24af9e8b820d94ad138175c22
Jade Delebecque <JadeDelebecque>
2025-05-27 14:19:32 +0100

    tentative de liaison a la bdd avortée

--- frontend_prog/app/routes.py
+++ frontend_prog/app/routes.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
 from .db_access import get_user_by_email_password
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'backend_prog')))
+
+from users.user import user
 
 main = Blueprint('main', __name__)
-
+user1 = user(1234, 'truc@mail.com', 'password', 'student')
+ 
 @main.route('/')
 def home():
     return render_template('home.html')
@@ -19,29 +25,27 @@ def teachers():
         return redirect(url_for('main.login'))
     return render_template('teachers.html')
 
-@main.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-
-        user = get_user_by_email_password(email, password)
-
-        if user:
-            session['user'] = {
-                'user_id': user[0],
-                'email': user[1],
-                'role': user[2]
-            }
-
-            if user[2] == 'student':
-                return redirect(url_for('main.students'))
-            elif user[2] == 'teacher':
-                return redirect(url_for('main.teachers'))
-            else:
-                error = "Access denied for this role."
-        else:
-            error = "Invalid email or password."
-
-    return render_template('login.html', error=error)
+# @main.route('/login', methods=['GET', 'POST'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         email = request.form['email']
+#         password = request.form['password']
+#
+#         if user1:
+#             session['user1'] = {
+#                 'user_id': user1[0],
+#                 'email': user1[1],
+#                 'role': user1[2]
+#             }
+#
+#             if user1[2] == 'student':
+#                 return redirect(url_for('main.students'))
+#             elif user1[2] == 'teacher':
+#                 return redirect(url_for('main.teachers'))
+#             else:
+#                 error = "Access denied for this role."
+#         else:
+#             error = "Invalid email or password."
+#
+#     return render_template('login.html', error=error)
