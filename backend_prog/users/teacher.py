8e646a7a829b0ea548761db64319fc72a1bce146
Carlos OKINDA Aka Annoying Floater <okindaceo@gmail.com>
2025-05-27 13:07:05 +0200

    class

---
+++ backend_prog/users/teacher.py
@@ -0,0 +1,34 @@
+import user
+class teacher(user):
+    
+    def create_election():
+        pass
+
+    def modify_election():
+        pass
+
+    def delete_election():
+        pass
+
+    def view_election_infos():
+        pass
+
+    def stop_election():
+        pass    
+
+    def pause_election():
+        pass
+
+    def add_student_to_election():
+        pass
+
+    def remove_student_from_election():
+        pass
+
+    def view_result_from_election():
+        pass
+
+    def publish_result_from_election():
+        pass
+
+    
--- backend_prog/users/user.py
+++ backend_prog/users/user.py
@@ -1,1 +1,27 @@
-print("test")
+class user: 
+    def __init__(self, user_id, username, password):
+        self.user_id = user_id
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f"User(user_id={self.user_id}, username='{self.username}')"
+
+    def to_dict(self):
+        return {
+            "user_id": self.user_id,
+            "username": self.username,
+            "password": self.password
+        }
+    
+    def login(self):
+        """
+        Connection to the user account.
+        """
+        pass
+
+    def logout(self):
+        """
+        Disconnect from the user account.
+        """
+        pass
